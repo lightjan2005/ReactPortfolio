3b7e8314e479a2a4a44f53e739e51d8a7beea63c
Kai Ying Chan <lightjan2005@gmail.com>
2022-06-12 22:35:20 -0700

    create navbar and information and animation css

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import {Person,Mail} from "@material-ui/icons"
 import "./topbar.scss"
-export default function Topbar() {
+export default function Topbar({menuOpen, setMenuOpen}) {
   return (
-    <div className='topbar'>
-        
+    <div className={'topbar ' + (menuOpen && 'active')}>
+        <div className="wrapper">
+          <div className="left">
+            <a href="#intro" className='logo'>Kai-Ying (Lite) Chan</a>
+            <div className="itemContainer">
+              <Person className="icon"/>
+              <span>+1 206-635-6326</span>
+            </div>
+            <div className="itemContainer">
+              <Mail className='icon'/>
+              <span>lightjan2005@gmail.com</span>
+            </div>
+          </div>
+          <div className="right">
+            <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
+              <span className='line1'></span>
+              <span className='line1'></span>
+              <span className='line1'></span>
+            </div>
+          </div>
+        </div>
         
     </div>
   )
