b18d1deab76fecff8691ded462c23114779192ef
Kai Ying Chan <lightjan2005@gmail.com>
2022-06-17 14:30:12 -0700

    finish portfolio and portfolioList component

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,85 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import "./portfolio.scss"
+import PortfolioList from '../portfolioList/PortfolioList'
+import{
+  allProjects,
+  webApp,
+  api,
+  database,
+  other
+} from "../../projects.js"
+
 export default function Portfolio() {
+  
+  const [selected,setSelected] = useState("all");
+  const [data, setdata] = useState([]);
+  const list = [
+    {
+      id: "all",
+      title: "All Projects",
+    },
+    {
+      id: "web",
+      title: "Web App",
+    },
+    {
+      id: "api",
+      title: "API",
+    },
+    {
+      id: "database",
+      title: "Database",
+    },
+    {
+      id: "other",
+      title: "Other",
+    },
+  ]
+
+  useEffect(()=>{
+    switch(selected){
+      case "all":
+        setdata(allProjects);
+        break;
+      case "web":
+        setdata(webApp);
+        break;
+      case "api":
+        setdata(api);
+        break;
+      case "database":
+        setdata(database);
+        break;
+      case "other":
+        setdata(other);
+        break;
+      default:
+        setdata(allProjects);
+    }
+
+  },[selected])
+
   return (
     <div className='portfolio' id="portfolio">
-        
+        <h1>Portfolio</h1>
+        <ul>
+          {list.map(item=>(
+            <PortfolioList 
+            title={item.title} 
+            active={selected === item.id} 
+            setSelected={setSelected}
+            id={item.id}
+            />
+          ))}
+        </ul>
+        <div className="container">
+          {data.map((d) =>(
+            <div className="item">
+              <img src={d.img} alt=""/>
+              <h3>{d.title}</h3>
+            </div>
+          ))}
+        </div>
     </div>
   )
 }
---
+++ src/projects.js
@@ -0,0 +1,135 @@
+export const allProjects =[
+    {
+        id: 1,
+        title: "JamoutApp",
+        img:"https://i.postimg.cc/Wz47Th0B/jamout.png",
+        repoURL:"",
+        websiteURL:"https://jamout.azurewebsites.net/#/"
+    },
+    {
+        id: 2,
+        title: "Basketball testing",
+        img:"https://i.postimg.cc/wMqdH8xt/Basketball.png",
+        repoURL:"https://github.com/lightjan2005/CPSC5210Team7",
+        websiteURL:""
+    },
+    {
+        id: 3,
+        title: "Image Processor API",
+        img:"https://i.postimg.cc/Kzc98ZLy/image-Process-API.png",
+        repoURL:"https://github.com/lightjan2005/imageProcessAPI",
+        websiteURL:""
+    },
+    {
+        id: 4,
+        title: "Address Searcher API",
+        img:"https://i.postimg.cc/g0bz57hh/Address-Search-API.png",
+        repoURL: "https://github.com/ChungNYCU/address-search-api",
+        websiteURL:""
+    },
+    {
+        id: 5,
+        title: "Pacman game",
+        img:"https://i.postimg.cc/TYZ5dCmd/pacman.jpg",
+        repoURL: "https://github.com/lightjan2005/AI"
+    },
+    {
+        id: 6,
+        title: "Traveling Fun Website",
+        img:"https://i.postimg.cc/fLcQdWqq/Traveling-Fun.png",
+        repoURL: "https://github.com/eliogilvy/NotUW.TravellingFan"
+    },
+    {
+        id: 7,
+        title: "Health Clinic Database",
+        img:"https://i.postimg.cc/zfMPfmJt/Health-Link.png",
+        repoURL: "https://github.com/lightjan2005/databaseSystems"
+    },
+    {
+        id: 8,
+        title: "JQuery Personal Website",
+        img:"https://i.postimg.cc/7PBwnhYj/Jquery-website.png",
+        repoURL: "https://github.com/lightjan2005/lightjan2005.GitHub.io",
+        websiteURL: "https://lightjan2005.github.io/"
+    },
+    {
+        id: 9,
+        title: "Taiwanese Website",
+        img:"https://i.postimg.cc/vmGNK70h/Taiwanese-website.png",
+        repoURL: "https://github.com/lwtech-edu/web-project-lightjan2005",
+        websiteURL:"http://ddion.com/htmlf19pm/Kai_Chan/"
+    },
+
+];
+
+export const webApp =[
+    {
+        id: 1,
+        title: "JamoutApp",
+        img:"https://i.postimg.cc/Wz47Th0B/jamout.png",
+        repoURL:"",
+        websiteURL:"https://jamout.azurewebsites.net/#/"
+    },
+    {
+        id: 2,
+        title: "Traveling Fun Website",
+        img:"https://i.postimg.cc/fLcQdWqq/Traveling-Fun.png",
+        repoURL: "https://github.com/eliogilvy/NotUW.TravellingFan"
+    },
+    {
+        id: 3,
+        title: "JQuery Personal Website",
+        img:"https://i.postimg.cc/7PBwnhYj/Jquery-website.png",
+        repoURL: "https://github.com/lightjan2005/lightjan2005.GitHub.io",
+        websiteURL: "https://lightjan2005.github.io/"
+    },
+    {
+        id: 4,
+        title: "Taiwanese Website",
+        img:"https://i.postimg.cc/vmGNK70h/Taiwanese-website.png",
+        repoURL: "https://github.com/lwtech-edu/web-project-lightjan2005",
+        websiteURL:"http://ddion.com/htmlf19pm/Kai_Chan/"
+    },
+];
+
+export const api =[
+    {
+        id: 1,
+        title: "Image Processor API",
+        img:"https://i.postimg.cc/Kzc98ZLy/image-Process-API.png",
+        repoURL:"https://github.com/lightjan2005/imageProcessAPI",
+        websiteURL:""
+    },
+    {
+        id: 2,
+        title: "Address Searcher API",
+        img:"https://i.postimg.cc/g0bz57hh/Address-Search-API.png",
+        repoURL: "https://github.com/ChungNYCU/address-search-api",
+        websiteURL:""
+    },
+];
+
+export const database =[
+    {
+        id: 1,
+        title: "Health Clinic Database",
+        img:"https://i.postimg.cc/zfMPfmJt/Health-Link.png",
+        repoURL: "https://github.com/lightjan2005/databaseSystems"
+    },
+];
+
+export const other =[
+    {
+        id: 1,
+        title: "Basketball testing",
+        img:"https://i.postimg.cc/wMqdH8xt/Basketball.png",
+        repoURL:"https://github.com/lightjan2005/CPSC5210Team7",
+        websiteURL:""
+    },
+    {
+        id: 2,
+        title: "Pacman game",
+        img:"https://i.postimg.cc/TYZ5dCmd/pacman.jpg",
+        repoURL: "https://github.com/lightjan2005/AI"
+    },
+];
