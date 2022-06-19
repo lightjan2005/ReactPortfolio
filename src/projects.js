7fc7290b483daba40c282765e2f5cae1d88ab524
Kai Ying Chan <lightjan2005@gmail.com>
2022-06-18 18:56:54 -0700

    add description and icon for projects

--- src/projects.js
+++ src/projects.js
@@ -2,62 +2,80 @@ export const allProjects =[
     {
         id: 1,
         title: "JamoutApp",
-        img:"https://i.postimg.cc/Wz47Th0B/jamout.png",
+        img:"https://i.postimg.cc/1XPszHV4/jamout.png",
         repoURL:"",
-        websiteURL:"https://jamout.azurewebsites.net/#/"
+        websiteURL:"https://jamout.azurewebsites.net/#/",
+        description:"",
+        icon:"../public/assets/globe.png",
     },
     {
         id: 2,
         title: "Basketball testing",
         img:"https://i.postimg.cc/wMqdH8xt/Basketball.png",
         repoURL:"https://github.com/lightjan2005/CPSC5210Team7",
-        websiteURL:""
+        websiteURL:"",
+        description:"",
+        icon:"",
     },
     {
         id: 3,
         title: "Image Processor API",
         img:"https://i.postimg.cc/Kzc98ZLy/image-Process-API.png",
         repoURL:"https://github.com/lightjan2005/imageProcessAPI",
-        websiteURL:""
+        websiteURL:"",
+        description:"",
+        icon:"",
     },
     {
         id: 4,
         title: "Address Searcher API",
         img:"https://i.postimg.cc/g0bz57hh/Address-Search-API.png",
         repoURL: "https://github.com/ChungNYCU/address-search-api",
-        websiteURL:""
+        websiteURL:"",
+        description:"",
+        icon:"",
     },
     {
         id: 5,
         title: "Pacman game",
         img:"https://i.postimg.cc/TYZ5dCmd/pacman.jpg",
-        repoURL: "https://github.com/lightjan2005/AI"
+        repoURL: "https://github.com/lightjan2005/AI",
+        description:"",
+        icon:"",
     },
     {
         id: 6,
         title: "Traveling Fun Website",
         img:"https://i.postimg.cc/fLcQdWqq/Traveling-Fun.png",
-        repoURL: "https://github.com/eliogilvy/NotUW.TravellingFan"
+        repoURL: "https://github.com/eliogilvy/NotUW.TravellingFan",
+        description:"",
+        icon:"../public/assets/globe.png",
     },
     {
         id: 7,
         title: "Health Clinic Database",
         img:"https://i.postimg.cc/zfMPfmJt/Health-Link.png",
-        repoURL: "https://github.com/lightjan2005/databaseSystems"
+        repoURL: "https://github.com/lightjan2005/databaseSystems",
+        description:"",
+        icon:"",
     },
     {
         id: 8,
         title: "JQuery Personal Website",
         img:"https://i.postimg.cc/7PBwnhYj/Jquery-website.png",
         repoURL: "https://github.com/lightjan2005/lightjan2005.GitHub.io",
-        websiteURL: "https://lightjan2005.github.io/"
+        websiteURL: "https://lightjan2005.github.io/",
+        description:"",
+        icon:"../public/assets/globe.png",
     },
     {
         id: 9,
         title: "Taiwanese Website",
         img:"https://i.postimg.cc/vmGNK70h/Taiwanese-website.png",
         repoURL: "https://github.com/lwtech-edu/web-project-lightjan2005",
-        websiteURL:"http://ddion.com/htmlf19pm/Kai_Chan/"
+        websiteURL:"http://ddion.com/htmlf19pm/Kai_Chan/",
+        description:"",
+        icon:"../public/assets/globe.png",
     },
 
 ];
